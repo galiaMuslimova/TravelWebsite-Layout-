b2d3ae039c07e8682eab4dd200d2e53546d0e3b0
Galia Muslimova <galia.musllimova@gmail.com>
2021-04-12 13:03:09 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const {src, dest, series} = require('gulp')
+const sass = require('gulp-sass')
+const include = require('gulp-file-include')
+const concat = require('gulp-concat')
+
+function html() {
+  return src('src/**.html')
+  .pipe(include({
+    prefix: '@@'
+  }))
+  .pipe(dest('root'))
+}
+
+function scss() {
+  return src('src/scss/**/*.scss')
+  .pipe(sass())
+  .pipe(concat('index.css'))
+  .pipe(dest('root'))
+}
+
+exports.build = series(html, scss)
